c433e1344ee68cb33ca2e14ab6d5d7aa277cded0
Szymon Bacanski <s.bacanski@gmail.com>
2024-06-07 00:33:00 +0200

    Validation input type
    Commits contains change source of the input validation for middleware which unpack request's body into response's locals variables.

--- src/middleware/unpackRequestBody.ts
+++ src/middleware/unpackRequestBody.ts
@@ -1,17 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
+import { GeneratedStringsInput } from '../utils/requestsValidation';
 
-// TODO: to delete \/
-// NOTE: this can be take from validation schema - schema can be transformed into type
-type EndpointRequestBody = {
-	lengths: {
-		minimal: number;
-		maximal: number;
-	};
-	characters: string;
-	combinations: number;
-};
-
-const unpackRequestBody = (req: Request<{}, {}, EndpointRequestBody>, res: Response, next: NextFunction) => {
+const unpackRequestBody = (req: Request<{}, {}, GeneratedStringsInput>, res: Response, next: NextFunction) => {
 	res.locals.minLength = req.body.lengths.minimal;
 	res.locals.maxLength = req.body.lengths.maximal;
 	res.locals.characters = req.body.characters;
