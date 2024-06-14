fb72f32cc53119d9637285bf8dbf6f64f53ffcce
Szymon Bacanski <s.bacanski@gmail.com>
2024-06-15 00:05:25 +0200

    hotfix/new-names-for-endpoints-paths

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const GENERATE_STRINGS_ENDPOINT_PATH = '/api/generate-strings-file/';
+export const GENERATE_STRINGS_ENDPOINT_PATH = '/api/generate-strings/';
 export const CURRENTLY_RUNNING_OPERATIONS_ENDPOINT_PATH = '/api/currently-running-operations/';
-export const RETURN_GENERATED_FILE_ENDPOINT_PATH = '/api/return-generated-files/:id';
+export const RETURN_GENERATED_FILE_ENDPOINT_PATH = '/api/return-generated-strings/:id';
